fded1b2719fc882f91890796279e7fd692221655
David Martínez <164534226+David-Martinez-1077@users.noreply.github.com>
2024-11-11 14:13:34 -0600

    Add files via upload

--- UIII_P1_CRUD_Tabla_Materia_v2/materia_app/views.py
+++ UIII_P1_CRUD_Tabla_Materia_v2/materia_app/views.py
@@ -13,3 +13,25 @@ def registrarMateria(request):
 
     guardarmateria = Materia.objects.create(codigo = codigo, nombre = nombre, credito = credito)
     return redirect("/") 
+
+
+def seleccionarMateria(request, codigo):
+    materia = Materia.objects.get(codigo=codigo)
+    return render(request, "editarmateria.html", {"mismaterias": materia})
+
+def editarMateria(request):
+    codigo = request.POST['txtcodigo']
+    nombre = request.POST['txtmateria']
+    credito = request.POST['numcreditos']
+
+    materia = Materia.objects.get(codigo=codigo)
+    materia.nombre = nombre
+    materia.credito = credito
+    materia.save()
+    return redirect("/") 
+
+def borrarMateria(request, codigo):
+    mat = Materia.objects.get(codigo=codigo)
+    mat.delete()
+    
+    return redirect("/")
